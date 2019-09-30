c2f866b3f4655917bbbc0ab6c0642b262fc78f45
SeyedRamin Rasoulinezhad <ramin_r348@yahoo.com>
2019-09-30 16:52:21 +1000

    update

--- preprocess.py
+++ preprocess.py
@@ -28,7 +28,7 @@ def scale_crop(input_size, scale_size=None, normalize=__imagenet_stats):
         transforms.Normalize(**normalize),
     ]
     if scale_size != input_size:
-        t_list = [transforms.Scale(scale_size)] + t_list
+        t_list = [transforms.Resize(scale_size)] + t_list
 
     return transforms.Compose(t_list)
 
@@ -40,7 +40,7 @@ def scale_random_crop(input_size, scale_size=None, normalize=__imagenet_stats):
         transforms.Normalize(**normalize),
     ]
     if scale_size != input_size:
-        t_list = [transforms.Scale(scale_size)] + t_list
+        t_list = [transforms.Resize(scale_size)] + t_list
 
     transforms.Compose(t_list)
 
